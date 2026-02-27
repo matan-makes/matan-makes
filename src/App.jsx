20eb239742f6a0baafaba3533d18cb11e20d8aa8
Tan U <dev.muchen@gmail.com>
2026-02-27 01:21:56 -0500

    Switch from BrowserRouter to HashRouter for routing in App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Privacy from './pages/Privacy'
 
-const basename = import.meta.env.BASE_URL.replace(/\/$/, '')
-
 function App() {
   return (
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <div className="flex flex-col min-h-screen bg-cloud text-navy font-sans">
         <Header />
         <Routes>
@@ -17,7 +15,7 @@ function App() {
         </Routes>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
